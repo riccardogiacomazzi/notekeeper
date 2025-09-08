085d8ed2547da830689f71721c3bbdaf454ce9bd
riccardogiacomazzi <riccardogiacomazzi93@gmail.com>
2025-09-08 21:20:46 +0200

    Defining styling home page

--- src/App.tsx
+++ src/App.tsx
@@ -52,28 +52,39 @@ function App() {
       />
 
       {/* MAPPING */}
-      <div className="py-12">
-        {/* search results */}
-        {isSearching && (
-          <SearchParameters
-            searchText={searchText}
-            testContent={testContent}
-            filteredContent={displayedContent}
-            currentSearch={currentSearch}
-            searchCriteria={searchCriteria}
-            setSearchCriteria={setSearchCriteria}
-          />
-        )}
-        <div className="pt-10 grid grid-cols-4 gap-4">
-          {displayedContent.map((item, index) => (
-            <div
-              key={index}
-              className="relative flex items-start aspect-square border mb-2 cursor-pointer hover:opacity-50 overflow-hidden"
-            >
-              {item.image && <img className="w-full h-full object-cover opacity-20" src={item.image} alt="" />}
-              <div className="absolute p-6 top-0 left-0">{item.content}</div>
-            </div>
-          ))}
+      <div className="w-full">
+        <div className="py-12 mx-4">
+          {/* search results */}
+          <div className=" flex items-center h-12">
+            {isSearching ? (
+              <SearchParameters
+                searchText={searchText}
+                testContent={testContent}
+                filteredContent={displayedContent}
+                currentSearch={currentSearch}
+                searchCriteria={searchCriteria}
+                setSearchCriteria={setSearchCriteria}
+              />
+            ) : (
+              <div className="flex text-left font-bold">
+                <div>Explore •</div>
+                <div> Explore •</div>
+                <div> Explore </div>
+              </div>
+            )}
+          </div>
+          {/* item grid */}
+          <div className="h-screen w-auto pt-10 grid grid-cols-4 gap-4">
+            {displayedContent.map((item, index) => (
+              <div
+                key={index}
+                className="relative flex items-start aspect-square border mb-2 cursor-pointer hover:opacity-50 overflow-hidden"
+              >
+                {item.image && <img className="w-full h-full object-cover opacity-20" src={item.image} alt="" />}
+                <div className="absolute p-6 top-0 left-0">{item.content}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
